ea0b53f0c5bbd362d0a2ada0f7cbfab0bf45b8be
HuynhThai <huynhb1605217@student.ctu.edu.vn>
2019-06-07 23:30:36 +0700

    commit

--- lib/menu.dart
+++ lib/menu.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 // Sua code thi update ben menu ben trai, duoi CHANGES la nhung file co thay doi, nhap message tren o message roi bam Ctrl + Enter de luu thay doi vo Git
-// Goc duoi ben trai ke chu master* bam vo mui ten xoay xoay de update code len Git      
+// Goc duoi ben trai ke chu master* bam vo mui ten xoay xoay de update code len Git
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,52 +10,43 @@ class Menu extends StatelessWidget {
       new AppBar(
         leading: Icon(Icons.menu),
       ),
-      
-      drawer: new Drawer (
+      drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
-        new IconButton(
-          icon: new Icon(Icons.search),
-          tooltip: 'search',
-          onPressed: null,
+            new IconButton(
+              icon: new Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: null,
+            ),
+            new ListTile(
+                title: new Text('Giới thiệu Đại Học Cần Thơ'),
+                trailing: new Icon(Icons.trending_flat)),
+            new ListTile(
+              title: new Text('Thông tin tuyển sinh'),
+              trailing: new Icon(Icons.trending_flat),
+            ),
+            new ListTile(
+              title: new Text('Danh sách ngành'),
+              trailing: new Icon(Icons.trending_flat),
+            ),
+            new ListTile(
+              title: new Text('Xét tuyển thẳng'),
+              trailing: new Icon(Icons.trending_flat),
+            ),
+            new Text('Thông tin liên hệ'),
+            new Text('Phòng Đào tạo - Trường Đại học Cần Thơ'),
+            new Text('- Địa Chỉ : Đường 3/2, Quận Ninh Kiều, TP.Cần Thơ'),
+            new Text('- Điện thoại : 0292.3872 728'),
+            new Text('- Email:tuyensinh@ctu.edu.vn'),
+            new Text('- Mobile/Zalo/Viber:0886889922'),
+            new Text('- Kênh tư vấn: http://www.facebook.com/ctu.tvts'),
+            new ListTile(
+              trailing: new Icon(Icons.arrow_back),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+          ],
         ),
-        new  ListTile(
-          title: new Text('Giới thiệu Đại Học Cần Thơ'),
-          trailing: new Icon(Icons.trending_flat)
-        ),
-        new ListTile(
-          title: new Text('Thông tin tuyển sinh'),
-          trailing: new Icon(Icons.trending_flat),
-        ),
-        new ListTile(
-          title: new Text('Danh sách ngành'),
-          trailing: new Icon(Icons.trending_flat),
-          
-        ),
-        new ListTile(
-          title: new Text('Xét tuyển thẳng'),
-          trailing: new Icon(Icons.trending_flat),
-        ),
-        new ListTile(
-          title: new Text('Thông tin liên hệ'),
-          child: new ListView(
-            children: <Widget>[
-          new Text('Phòng Đào tạo - Trường Đại học Cần Thơ'),
-          new Text('- Địa Chỉ : Đường 3/2, Quận Ninh Kiều, TP.Cần Thơ'),
-          new Text('- Điện thoại : 0292.3872 728'),
-          new Text('- Email:tuyensinh@ctu.edu.vn'),
-          new Text('- Mobile/Zalo/Viber:0886889922'),
-          new Text('- Kênh tư vấn: http://www.facebook.com/ctu.tvts'),
-            ],
-          )
-          ),
-        new ListTile(
-          trailing: new Icon(Icons.arrow_back),
-          onTap: ()=> Navigator.of(context).pop(),
-        ),
-        ],
-        ),
-        ), 
+      ),
     );
   }
 }
