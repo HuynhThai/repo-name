66898d1e6abbc0de3be1787329f33d52cbd984fc
HuynhThai <huynhb1605217@student.ctu.edu.vn>
2019-06-15 01:56:00 +0700

    UI designing home

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hello_world/menu.dart';
 
@@ -7,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: new ThemeData(primarySwatch: Colors.blue),
+      theme: new ThemeData(primarySwatch: Colors.blueGrey ),
       home: new Home(),
+
     );
   }
 }
@@ -17,13 +19,114 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: new AppBar(
-        title: Text(""),
+      title: Text(" TUYỂN SINH CTU"),
       ),
-      body: Container(
-        child: Text(""),
-      ),
+      body:Column(children: <Widget>[
+        HomeScreen(),
+        //HomeScreenpart(),
+        Row(
+    mainAxisSize : MainAxisSize.max,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children:<Widget>[
+      Text("Cập nhật tin tức" ),
+      Spacer(),
+      Text("xem >>", style: viewAllStyle ,)
+      
+    ],
+  ),
+      ],),
       drawer: new Drawer(child: Menu()),
     );
+    
+  }
+}
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+  Widget build(BuildContext context){
+    return Scaffold(
+    );
+    }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 200.0, color: Colors.blueGrey,
+        ),
+      Padding(padding: EdgeInsets.all(20.0),
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        child : TextField(
+          decoration: InputDecoration(
+           border: InputBorder.none,
+           hintText: '  Enter a search term',
+           suffixIcon: Material(
+             elevation: 1.0,
+             borderRadius: BorderRadius.all(Radius.circular(20.0)),
+             child: Icon(Icons.search, color:Colors.black),
+           )
+           ),
+        ),
+      ),
+      )
+      ],
+    );
+  }
+}
+var viewAllStyle = TextStyle( fontSize: 14.0 );
+var homeScreenpart = Column(children: <Widget>[
+  Padding(padding: const EdgeInsets.all(8.0),
+  child: Row(
+    mainAxisSize : MainAxisSize.max,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children:<Widget>[
+      ListView(
+    scrollDirection: Axis.horizontal,
+    children: <Widget>[
+      Container( height:192.0),  
+      Container( height:192.0),
+      Container( height:192.0,),
+    ],
+  ) 
+    ],
+  ),
+  ),
+
+],
+);
+
+List<CityCard> cCard =[
+  CityCard (" assets/lib/images/1.jpg"),
+  CityCard (" assets/lib/images/2.png"),
+  CityCard (" assets/lib/images/3.jpg"),
+];
+
+class CityCard extends StatefulWidget{
+    final String imagePath;
+  CityCard (this.imagePath);
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(  
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(image: DecorationImage(image:AssetImage(imagePath))),
+            child: Image.asset(imagePath)
+            ,)
+        ],
+      ),
+    );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return null;
   }
 }
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,6 +1,16 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-class Menu extends StatelessWidget {
+//import 'package:webview_flutter/webview_flutter.dart';
+class Menu extends StatefulWidget {
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -11,42 +21,44 @@ class Menu extends StatelessWidget {
         ),
         new Padding(
           padding: EdgeInsets.all(4.0)),
-          Container(
-            child: WebView(
-              initialUrl: 'https://www.ctu.edu.vn/',
-              javascriptMode: JavascriptMode.unrestricted,
-            )
-          ),
+         // Container(
+           // child: WebView(
+           // initialUrl: 'https://www.ctu.edu.vn/',
+           //   javascriptMode: JavascriptMode.unrestricted,
+           // )
+          //),
         new ListTile(
           leading: new Icon(Icons.record_voice_over),
           title: new Text('Thông tin tuyển sinh'),
         ),
         new Padding(
           padding: EdgeInsets.all(4.0)),
-          Container(
-            child: WebView(
-              initialUrl: ' https://tuyensinh.ctu.edu.vn/dai-hoc-chinh-quy/thong-tin-tuyen-sinh.html',
-              javascriptMode: JavascriptMode.unrestricted,
-            ),
-          ),
+          //Container(
+            //child: WebView(
+              //initialUrl: ' https://tuyensinh.ctu.edu.vn/dai-hoc-chinh-quy/thong-tin-tuyen-sinh.html',
+              //javascriptMode: JavascriptMode.unrestricted,
+            //),
+          //),
         new ListTile(
           leading: new Icon(Icons.list),
           title: new Text('Danh sách ngành'),
         ),
+        new Padding(
+          padding: EdgeInsets.all(4.0)),        
         new ListTile(
           leading: new Icon(Icons.more),
           title: new Text('Xét tuyển thẳng'),
         ),
         new Padding(
           padding: EdgeInsets.all(4.0)),
-          Container(
-            child: WebView(
-              initialUrl: 'https://tuyensinh.ctu.edu.vn/chuong-trinh-dai-tra/793-xet-tuyen-thang.html',
-              javascriptMode: JavascriptMode.unrestricted,
-            )
-          ),
+          //Container(
+            //child: WebView(
+            //initialUrl: 'https://tuyensinh.ctu.edu.vn/chuong-trinh-dai-tra/793-xet-tuyen-thang.html',
+            //javascriptMode: JavascriptMode.unrestricted,
+            //)
+         // ),
          new ListTile(
-          trailing: new Icon(Icons.mail_outline),
+          leading: new Icon(Icons.mail_outline),
           title: Text('Thông tin liên hệ'),
           subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -59,22 +71,38 @@ class Menu extends StatelessWidget {
             ],
           ),
         ),
+        new Padding(
+          padding: EdgeInsets.all(5.0)),
       ],
     );
   }
-    
-   
 }
-//class WebView extends State<Menu> {
-  //Completer<WebViewController> _controller= completer<WebviewController>();
-  //final Set<String> _favorites = Set<String>();
-  //@override
-  ///Widget build(BuildContext context){
-    //return WebView(
-         //initialUrl: 'https://www.ctu.edu.vn/',
-         //javacriptMode:JavascriptMode.unrestricted,
-         //onWebViewCreated:(WebViewController webViewController){
-        //_controller.complete(webViewController);
-   //);
- // }
-//}
+class WebView extends State<Menu> {
+  final Completer<WebViewController> _controller= completer<WebviewController>();
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebView(
+        initialUrl: 'https://www.ctu.edu.vn/',
+         javacriptMode: JavascriptMode.unrestricted,
+         onWebViewCreated:(WebViewController webViewController){
+        _controller.complete(webViewController);
+      ),
+    floatingActionButton: FutureBuilder<WebViewController>(
+      future: _controller.future ,
+      builder: (BuildContext context, AsyncSnapshot<WebViewController> controller),{
+        if (controller.hasData) {
+          return FloatingActionButton(
+            onPressed: () async{
+              controller.data.reload();
+
+            },
+            child: const Icon(Icon.refesh),
+            );   
+        }
+        return Container();
+      }    
+   );
+  }
+}
